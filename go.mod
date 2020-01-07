4872e02ec25a9cd1c93c6baf31eff93d774abca8
Boy Baukema <bbaukema@veracode.com>
2020-01-07 09:36:32 +0100

    Added import to external dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/relaxnow/go-modules-example
 
 go 1.12
+
+require rsc.io/quote v1.5.2
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-
+	"rsc.io/quote"
 	"github.com/relaxnow/go-modules-example/stringutil"
 )
 
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(quote.Hello())
 }
